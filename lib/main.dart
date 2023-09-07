6a4e687cf88e3712e4a1c5667de429bb6b48bad8
Sahilali98 <sahilali.ind@gmail.com>
2023-09-07 09:41:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: Container(
+          child: Column(
+            children: const [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(
+                    'images/WhatsApp Image 2022-04-13 at 8.11.59 AM.jpeg'),
+              ),
+              Text(
+                "SK SAHIL ALI",
+                style: TextStyle(
+                  fontFamily: "SquarePeg",
+                  fontSize: 60.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVLOPER",
+                style: TextStyle(
+                  fontFamily: "",
+                  fontSize: 60.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    "9337809365",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0,
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    "sahilali.ind@gmail.com",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.0,
+                        fontSize: 20.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
